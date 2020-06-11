3d1bdcd0243edb54d9248b32f72dc7d3b10820ba
BarOvda <barovde55@gmail.com>
2020-06-11 21:40:18 +0300

    navigation bar

--- app/src/main/java/com/example/games4you/MainActivity.java
+++ app/src/main/java/com/example/games4you/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.games4you;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.Console;
+
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    TextView mEmailText;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+         user = FirebaseAuth.getInstance().getCurrentUser();
+        setContentView(R.layout.nav_header);
+        mEmailText = findViewById(R.id.navMenuEmailDisplay);
+
+        mEmailText.setText("user.getEmail()");
+
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer((GravityCompat.START));
+        }else{
+            super.onBackPressed();
+        }
     }
 }
