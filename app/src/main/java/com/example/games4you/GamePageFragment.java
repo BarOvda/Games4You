8b3a8b39c4a1d86b131d8c188ec05f5a659940ab
Daniel <daniel.zusev@gmail.com>
2020-07-12 11:32:27 +0300

    d

--- app/src/main/java/com/example/games4you/GamePageFragment.java
+++ app/src/main/java/com/example/games4you/GamePageFragment.java
@@ -4,22 +4,97 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.games4you.logic.Game;
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class GamePageFragment extends Fragment {
 
     public static final String YOUTUBE_CRED = "AIzaSyBsgtxTZEPQvf1_M34bJCmwlL1ROcJGu7c";
 
     private YouTubeBaseActivity youTubeBaseActivity;
+    private YouTubePlayerView youTubePlayerViewTrial;
+    private YouTubePlayerView youTubePlayerViewGamePlay;
+    private YouTubePlayer.OnInitializedListener onInitializedListenerTrial;
+    private YouTubePlayer.OnInitializedListener onInitializedListenerGamePlay;
+    private Button btnPlayTrial;
+    private Button btnPlayGamePlay;
+    private Game mGame;
+    private TextView description;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game_page,container,false);
+        View view = inflater.inflate(R.layout.fragment_ps4, container, false);
+        youTubeBaseActivity = new YouTubeBaseActivity();
+        youTubePlayerViewTrial = youTubeBaseActivity.findViewById(R.id.trial_youtube);
+        youTubePlayerViewGamePlay = youTubeBaseActivity.findViewById(R.id.gameplay_youtube);
+        btnPlayTrial = view.findViewById(R.id.play_trial_btn);
+        btnPlayGamePlay = view.findViewById(R.id.play_gameplay_btn);
+        description = view.findViewById(R.id.game_description);
+
+        //============ get game from other activity\fragment ================
+        Bundle bundle = this.getArguments();
+        mGame = (Game) bundle.getSerializable("game");
+        assert mGame != null;
+        description.setText(mGame.getmDescription());
+
+        //===================================================================
+
+        onInitializedListenerTrial = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(mGame.getmTrailer());
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(getContext(), "Video Failed To Load", Toast.LENGTH_LONG);
+            }
+        };
+
+        onInitializedListenerGamePlay = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadPlaylist(mGame.getmGamePlay());
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(getContext(), "Video Failed To Load", Toast.LENGTH_LONG);
+            }
+        };
+
+        btnPlayTrial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerViewTrial.initialize(YOUTUBE_CRED, onInitializedListenerTrial);
+            }
+        });
+
+        btnPlayGamePlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerViewGamePlay.initialize(YOUTUBE_CRED, onInitializedListenerGamePlay);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
--- app/src/main/java/com/example/games4you/logic/Game.java
+++ app/src/main/java/com/example/games4you/logic/Game.java
@@ -1,22 +1,29 @@
 package com.example.games4you.logic;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class Game {
+public class Game implements Serializable {
 
     private String mName;
     private String mImageUrl;
     private List<Categories> mCategories;
+    private String mDescription;
+    private String mTrailer;
+    private String mGamePlay;
     public Game(){
 
     }
 
-    public Game(String mName, String mImageUrl) {
+    public Game(String mName, String mImageUrl, String mDescription, String mTrailer, String mGamePlay) {
         this.mName = mName;
         this.mImageUrl = mImageUrl;
         this.mCategories = new ArrayList<>();
+        this.mDescription = mDescription;
+        this.mTrailer = mTrailer;
+        this.mGamePlay = mGamePlay;
     }
 
     public String getName() {
@@ -47,4 +54,10 @@ public class Game {
     public void addCategories(List<Categories> Categories){
         this.mCategories.addAll(Categories);
     }
+
+    public String getmDescription() { return mDescription; }
+
+    public String getmTrailer() { return mTrailer; }
+
+    public String getmGamePlay() { return mGamePlay; }
 }
